862da2761369b21f1befbb23ebb66983bdd4e9bf
nagaraju <chillapalli89@gmail.com>
2022-08-01 16:16:31 +0530

    Update app.component.ts
    first change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'firstApp-welcome to angular from Naga';
 }
